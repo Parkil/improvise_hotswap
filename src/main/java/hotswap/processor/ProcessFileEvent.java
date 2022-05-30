be66aa27323ee256b2a36955567dce0e97192921
Parkil <alkain77@gmail.com>
2022-05-30 10:16:21 +0900

    FileWatchEvent 정제 로직 + 테스트 케이스 추가

--- src/main/java/hotswap/processor/ProcessFileEvent.java
+++ src/main/java/hotswap/processor/ProcessFileEvent.java
@@ -1,12 +1,16 @@
 package hotswap.processor;
 
+import dto.EventDetail;
 import dto.FileWatchEvent;
+import dto.enum_vo.WatchEventType;
 import hotswap.event_queue.FileEventBlockingQueue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ProcessFileEvent {
 
@@ -22,8 +26,16 @@ public class ProcessFileEvent {
             collectedEventList.add(fileWatchEvent);
         }
 
-//        collectedEventList.stream().flatMap(watchEvent -> )
-
         return collectedEventList;
+    }
+
+    public List<String> getTargetFileNameList(List<FileWatchEvent> fileWatchEvents) {
+        Map<String, List<EventDetail>> groupByContextMap = fileWatchEvents.stream()
+                .flatMap(event -> event.getEventDetailList().stream())
+                .collect(Collectors.groupingBy(EventDetail::getContext));
+
+        return groupByContextMap.entrySet().stream()
+                .filter(entry -> entry.getValue().stream().noneMatch(listRow -> listRow.getWatchEventType() == WatchEventType.ENTRY_DELETE))
+                .map(Map.Entry::getKey).collect(Collectors.toList());
     }
 }
--- src/test/java/hotswap/watcher/WatchEventProcessTest.java
+++ src/test/java/hotswap/watcher/WatchEventProcessTest.java
@@ -1,6 +1,7 @@
 package hotswap.watcher;
 
 import dto.FileWatchEvent;
+import hotswap.processor.ProcessFileEvent;
 import hotswap.watcher.util.TestCommonUtil;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,14 +18,31 @@ class WatchEventProcessTest {
 
     private final Logger logger = LoggerFactory.getLogger(WatchEventProcessTest.class);
 
-    @Test
-    @DisplayName("단일 이벤트 처리 테스트")
-    void singleEventProcessTest() throws IOException {
-        Path mockFilePath = Path.of("src", "test", "java", "hotswap", "watcher", "mock_json_data", "single_event.json");
-        List<FileWatchEvent> mockList = TestCommonUtil.getObjectFromJson(mockFilePath);
+    private List<String> refineEvent(Path targetPath) throws IOException {
+        List<FileWatchEvent> mockList = TestCommonUtil.getObjectFromJson(targetPath);
 
         logger.info("mockList : {}", mockList);
 
-        assertEquals(1, mockList.size());
+        ProcessFileEvent processFileEvent = new ProcessFileEvent();
+        return processFileEvent.getTargetFileNameList(mockList);
+    }
+
+    @Test
+    @DisplayName("이벤트 처리 리스트 - 단일 이벤트")
+    void singleEventProcessTest() throws IOException {
+        Path mockFilePath = Path.of("src", "test", "java", "hotswap", "watcher", "mock_json_data", "single_event.json");
+        List<String> targetNameList = refineEvent(mockFilePath);
+
+        assertEquals(1, targetNameList.size());
+    }
+
+    @Test
+    @DisplayName("이벤트 처리 리스트 - 다중 이벤트 - Delete 대상 필터링")
+    void multiEventProcessTest() throws IOException {
+        Path mockFilePath = Path.of("src", "test", "java", "hotswap", "watcher", "mock_json_data", "multi_event.json");
+        List<String> targetNameList = refineEvent(mockFilePath);
+
+        assertEquals("fdsfds.java", targetNameList.get(0));
+        assertEquals(1, targetNameList.size());
     }
 }
